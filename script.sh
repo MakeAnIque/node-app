cec15ca04b9da76ddddec5f20564fa3f2dcbf630
Amitabh <32864393+MakeAnIque@users.noreply.github.com>
2024-08-14 12:49:46 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,7 @@
-echo $TARGET
+if [[ "$TARGET" = "my_pass" ]]; then
+    # Code to execute if strings are equal
+    echo "Pass is compared"
+else
+    # Code to execute if strings are not equal
+    echo "Pass is not compared"
+fi
